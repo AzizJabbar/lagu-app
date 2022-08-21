afb800356ea29018c1e8090b1988881a6c5b6318
AzizJabbar <azizjabbar1412@gmail.com>
2022-08-21 13:50:15 +0700

    update security policy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Layout from "./components/layout";
 function App() {
   return (
     <Layout>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
       <TopTracks />
       <TopArtists />
     </Layout>
