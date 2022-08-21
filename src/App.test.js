bc73f37fe31b58d9178fb753d82c41a7c7d47c31
AzizJabbar <azizjabbar1412@gmail.com>
2022-08-21 11:27:27 +0700

    add render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the landing page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("Top Tracks")).toBeInTheDocument;
+  expect(screen.getByText("Top Artists")).toBeInTheDocument;
+  expect(screen.getByPlaceholderText("Find your favorite song")).toBeInTheDocument;
+  expect(screen.getByPlaceholderText("Find your favorite artist")).toBeInTheDocument;
 });
