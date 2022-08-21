dede2644b9b66ae09174878a346ca1a750a452df
AzizJabbar <azizjabbar1412@gmail.com>
2022-08-21 13:54:51 +0700

    update security policy

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import TopTracks from "./containers/toptracks";
 import TopArtists from "./containers/topartists";
 import Layout from "./components/layout";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <Layout>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-
+      <Helmet>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Helmet>
       <TopTracks />
       <TopArtists />
     </Layout>
